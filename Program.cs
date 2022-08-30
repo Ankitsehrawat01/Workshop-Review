46d6cf4aeafe5ba918962b2182c43602004c3a88
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-30 09:33:05 +0530

    Summ adds Zero

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using WorkshopReview;
+
+namespace WorkshoReview
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int[] arr = { 0, -1, 2, -3, 1 };
+            int n = arr.Length;
+            Number myobj = new Number();
+            myobj.FindTriplets(arr, n);
+        }
+    }
+}
